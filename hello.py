7da30be8beae25d6bb21da9ca6ba241a405db622
suningqian <49641941+suningqian@users.noreply.github.com>
2020-01-22 13:03:30 +0800

    '3c'

--- hello.py
+++ hello.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -13,7 +23,6 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    # return '<h1>hello %s!</h1>' % name
     return render_template('user.html', name=name)
 
 
