ab9b8b520144e63a9cac1061030071f776d2fcb6
suningqian <49641941+suningqian@users.noreply.github.com>
2020-01-30 11:27:59 +0800

    '4b'

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -15,7 +15,7 @@ moment = Moment(app)
 
 
 class NameForm(FlaskForm):
-    name = StringField("What is your name?", validators=[DataRequired])
+    name = StringField("What is your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 
@@ -31,12 +31,11 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 if __name__ == '__main__':
