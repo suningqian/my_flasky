f3f3afcfc651fcc9f136dcadf07b4925733d2a6a
suningqian <49641941+suningqian@users.noreply.github.com>
2020-02-08 17:08:12 +0800

    '8a'

--- app/__init__.py
+++ app/__init__.py
@@ -24,4 +24,4 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    return  app
+    return app
--- flasky.py
+++ flasky.py
@@ -1,10 +1,13 @@
 import os
 import click
-from flask_migrate import Migrate
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Shell
+
 from app import create_app, db
 from app.models import User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
 migrate = Migrate(app, db)
 
 
@@ -23,3 +26,10 @@ def test(test_names):
     else:
         tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
